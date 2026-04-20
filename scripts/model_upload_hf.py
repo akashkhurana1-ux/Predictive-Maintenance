ba63054f9bb4bc720e54afad2188b0b1b3f6b39f
akashkhurana1-ux <akash.khurana1@gmail.com>
2026-04-21 01:46:22 +0530

    Update model_upload_hf.py

--- scripts/model_upload_hf.py
+++ scripts/model_upload_hf.py
@@ -3,8 +3,8 @@ from huggingface_hub import HfApi
 api = HfApi()
 
 api.upload_file(
-    path_or_fileobj="model/predive-maintenance_model.pkl",
-    path_in_repo="predictive-maintenance_model.pkl",
-    repo_id="akhurana-hf/predictive_maintenance/engine-failure-model",
+    path_or_fileobj="model/predictive_maintenance_model.pkl",
+    path_in_repo="predictive_maintenance_model.pkl",
+    repo_id="akhurana-hf/Predictive-Maintenance-Model",
     repo_type="model"
 )
